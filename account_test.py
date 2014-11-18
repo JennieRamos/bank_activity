d9f3a778327898cf2a5f796b645cda4748f96977
JennieRamos <jennieramos06@gmail.com>
2014-11-18 18:32:45 +0800

    first commit

---
+++ account_test.py
@@ -0,0 +1,19 @@
+import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+  def test_account_object_can_be_created(self):
+    account = Account()
+
+class TestAccount(unittest.TestCase):
+  def test_account_object_returns_current_balance(self):
+    account = Account("001", 50)
+    self.assertEqual(account.account_number, "001")
+    self.assertEqual(account.balance, 50)
+  
+def withdraw_from_account(self):
+  withdraw = withdraw()
+ 
+
+if  __name__== '__main__':
+  unittest.main()
