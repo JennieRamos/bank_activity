904d163412f96913602adf0c224aa5cb444e2267
JennieRamos <jennieramos06@gmail.com>
2014-11-18 20:52:18 +0800

    modified

--- account.py
+++ account.py
@@ -2,4 +2,7 @@ class Account(object):
   def __init__(self, account_number, balance):
     self.account_number = account_number
     self.balance = balance
-  
+
+class Withdraws(object):
+  def __init__( self, acount_number,balance,withdraw):
+    self.balance = balance-withdraw
--- account_test.py
+++ account_test.py
@@ -12,10 +12,9 @@ class TestAccount(unittest.TestCase):
     self.assertEqual(account.balance, 50)
  
   def test_withdraw(self):
-    account = account("001",20)
-    self.assertEqual(account.account_number, "001")
-    self.assertEqual(account.balance, 50)
-    self.assertEqual(account.withdraw, 30)
+    account = Withdraws("001", 50, 20)
+    self.assertEqual(account.balance, 30)
+    
 
 if __name__ == '__main__':
   unittest.main()
