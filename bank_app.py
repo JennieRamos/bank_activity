2f0fbc9947af930d3cafbec2ed87fdc98334bdfe
JennieRamos <jennieramos06@gmail.com>
2014-12-04 20:30:21 +0800

    edit bank-app

--- bank_app.py
+++ bank_app.py
@@ -1,20 +1,20 @@
-from flask import Flask , render_template, request
-from bank.bank import Bank
-from bank.account import Account
-app = Flask(__name__)
+"""This is docstring"""
+
+from flask import Flask, render_template, request
+from bank_activity.bank.bank import Bank
+from bank_activity.bank.account import Account
+APP = Flask(__name__)
 BANK = Bank()
 import cProfile
 
-@app.route('/')
+@APP.route('/')
 def hello_world():
+    """This is docstring"""
     account_number = request.args.get('account_number')
     balance = BANK.get_account_balance(account_number)
     return render_template('index.html', balance=balance)
 
-    
-
-    
 if __name__ == '__main__':
-    account = Account('1111', 50)
-    BANK.add_account(account)
-    cProfile.run('app.run(debug=True)', sort='time')
+    #account = Account('1111', 50)
+    BANK.add_account(Account('1111', 50))
+    cProfile.run('APP.run(debug=True)', sort='time')
