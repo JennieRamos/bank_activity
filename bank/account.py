7753532a202055df31802676e3e70809048d4294
JennieRamos <jennieramos06@gmail.com>
2014-12-11 19:50:10 +0800

    edit

--- bank/account.py
+++ bank/account.py
@@ -1,17 +1,10 @@
 class Account(object):
     """account"""
     def __init__(self, account_number, balance):
-      self.account_number = account_number
-      self.balance = balance
+        self.account_number = account_number
+        self.balance = balance
 
 class Withdraws(object):
     """withdraw"""
-    def __init__( self, acount_number,balance,withdraw):
-      self.balance = balance-withdraw
-
-#class Check_balance(object):  
-  #def __init__(self, balance):
-    #if type(balance) == int:
-      #return balance
-    #else:
-      #raise TypeError("Invalid type: {}".format(type(balance)))      
+    def __init__( self, acount_number, balance, withdraw):
+        self.balance = balance-withdraw
