af905d07f5104a7e48159747515df5f428440174
JennieRamos <jennieramos06@gmail.com>
2014-12-11 20:19:35 +0800

    edit

--- bank/account.py
+++ bank/account.py
@@ -10,12 +10,9 @@ class Account(object):
     def test3(self):
         """test3"""
         pass
- 
-"""class to the withdraw"""
 class Withdraws(object):
     """withdraw"""
-    def __init__(self, acount_number, balance, withdraw):
-        a = acount_number
+    def __init__(self, balance, withdraw):
         self.balance = balance-withdraw
     def test2(self):
         """test2"""
