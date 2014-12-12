5773494e6a93392728f512ec1363e0466f64de28
JennieRamos <jennieramos06@gmail.com>
2014-12-12 12:29:08 +0800

    fix

--- bank/account.py
+++ bank/account.py
@@ -4,19 +4,8 @@ class Account(object):
     def __init__(self, account_number, balance):
         self.account_number = account_number
         self.balance = balance
-    def test1(self):
-        """test1"""
-        pass
-    def test3(self):
-        """test3"""
-        pass
 class Withdraws(object):
     """withdraw"""
     def __init__(self, balance, withdraw):
         self.balance = balance-withdraw
-    def test2(self):
-        """test2"""
-        pass
-    def test4(self):
-        """test4"""
-        pass
+    
